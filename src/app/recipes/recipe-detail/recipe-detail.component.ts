f5df2d6e6a43309449e6f4d9ae96c4a2f8097942
nikhil756130 <nikhilkumarbehera18@gmail.com>
2023-10-13 18:58:55 +0530

    first commit

---
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
+import { ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+  selector: 'app-recipe-detail',
+  templateUrl: './recipe-detail.component.html',
+  styleUrls: ['./recipe-detail.component.css']
+})
+export class RecipeDetailComponent implements OnInit {
+  recipe: Recipe;
+  id: number;
+
+  constructor(private recipeService: RecipeService,
+    private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.id = +params['id'];
+        }
+      );
+  }
+
+  onAddToShoppingList() {
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+}
